0b7ed3e2168ac66c4bab0a61c969dc65f695471e
Geoffrey Challen <challen@illinois.edu>
2021-06-29 14:48:30 -0500

    Update dependencies and fix bug with Kotlin solution.

--- src/main/java/com/examples/addone/correct/kotlin/Question.kt
+++ src/main/java/com/examples/addone/correct/kotlin/Question.kt
@@ -1,5 +1,3 @@
-@file:AlsoCorrect
-
 package com.examples.addone.correct.kotlin
 
 import edu.illinois.cs.cs125.questioner.lib.AlsoCorrect
@@ -8,6 +6,12 @@ import edu.illinois.cs.cs125.questioner.lib.AlsoCorrect
  * Write a method `addOne` that returns its `Int` argument plus one.
  */
 
-fun addOne(value: Int): Int {
-  return value + 1
+@AlsoCorrect
+class Question {
+  // Note that this question was originally set up a bit differently in the demo video.
+  // Now Kotlin solutions that use @file:AlsoCorrect will not be templated when @Wrap is used on the solution.
+  // So the Kotlin solution should follow the class design established by the Java code, even for faux-static methods.
+  fun addOne(value: Int): Int {
+    return value + 1
+  }
 }
