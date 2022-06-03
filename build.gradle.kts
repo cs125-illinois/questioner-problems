eef06185dacaf2e63a844d44ed96daaec20dcc52
Geoffrey Challen <challen@illinois.edu>
2022-06-03 15:11:49 -0500

    Commit.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ tasks.withType<Test> {
   jvmArgs(
     "-ea", "-Xms4G", "-Xmx8G", "--enable-preview", "-XX:+UseZGC",
     "-Dfile.encoding=UTF-8",
-    "--add-opens java.base/java.lang=ALL-UNNAMED",
-    "--add-opens java.base/java.util=ALL-UNNAMED",
+    "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+    "--add-opens", "java.base/java.util=ALL-UNNAMED",
     "--add-exports", "jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
     "--add-exports", "jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
     "--add-exports", "jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED",
