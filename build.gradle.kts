32cada5f3fea9b4254a267dca027a2ae63590d38
Geoffrey Challen <challen@illinois.edu>
2022-07-23 16:44:40 -0500

    Bump dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
   checkstyle
   id("com.github.sherter.google-java-format") version "0.9"
   id("org.jmailen.kotlinter") version "3.11.1"
-  id("com.github.cs125-illinois.questioner") version "2022.7.3"
+  id("com.github.cs125-illinois.questioner") version "2022.7.4"
   id("com.github.ben-manes.versions") version "0.42.0"
   id("io.gitlab.arturbosch.detekt") version "1.20.0"
 }
@@ -19,7 +19,7 @@ repositories {
 }
 dependencies {
   implementation(kotlin("stdlib"))
-  implementation("com.github.cs125-illinois.questioner:lib:2022.7.3")
+  implementation("com.github.cs125-illinois.questioner:lib:2022.7.4")
 }
 tasks.withType<Test> {
   useJUnitPlatform()
