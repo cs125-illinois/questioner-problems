79b997ae68e4d0b32d74c9f51f494a5f0681b74c
Geoffrey Challen <challen@illinois.edu>
2022-09-29 15:52:48 -0500

    Dependency update.

---
+++ src/main/java/com/examples/withemptyconstructor/Question.java
@@ -0,0 +1,16 @@
+package com.examples.withemptyconstructor;
+
+import edu.illinois.cs.cs125.questioner.lib.Correct;
+
+/*
+ * Test to make sure we can parse the empty constructor.
+ */
+
+@Correct(name = "Test Empty Constructor", version = "2022.9.0", author = "challen@illinois.edu")
+public class Question {
+  private int value = 0;
+
+  int next() {
+    return value++;
+  }
+}
---
+++ src/main/java/com/examples/withemptyconstructor/correct/java/emptyconstructor/Question.java
@@ -0,0 +1,16 @@
+package com.examples.withemptyconstructor.correct.java.emptyconstructor;
+
+import edu.illinois.cs.cs125.questioner.lib.AlsoCorrect;
+
+@AlsoCorrect
+public class Question {
+  private int value;
+
+  public Question() {
+    value = 0;
+  }
+
+  int next() {
+    return value++;
+  }
+}
