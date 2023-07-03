81fcd4dc9c2f2955c55d2372e07868444913969c
Geoffrey Challen <challen@illinois.edu>
2023-07-02 21:00:35 -0500

    Small fixes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
   checkstyle
   id("com.github.sherter.google-java-format") version "0.9"
   id("org.jmailen.kotlinter") version "3.13.0"
-  id("com.github.cs124-illinois.questioner") version "2023.6.4"
+  id("com.github.cs124-illinois.questioner") version "2023.7.0"
   id("com.github.ben-manes.versions") version "0.47.0"
   id("io.gitlab.arturbosch.detekt") version "1.23.0"
 }
@@ -18,7 +18,7 @@ repositories {
   maven("https://maven.codeawakening.com")
 }
 dependencies {
-  implementation("com.github.cs124-illinois.questioner:lib:2023.6.4")
+  implementation("com.github.cs124-illinois.questioner:lib:2023.7.0")
 }
 tasks.withType<Test> {
   useJUnitPlatform()
